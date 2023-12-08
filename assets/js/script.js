88a67e9ee96abf284c0144dc449cb0cf27c5ee74
AlenaGM <alena.guillaume4@gmail.com>
2023-12-08 14:51:12 +0100

    fixed customized imputs clearing

--- assets/js/script.js
+++ assets/js/script.js
@@ -104,68 +104,60 @@ document.querySelector("#phone").addEventListener("change", function () {
 });
 
 //2- UX: ПРИ ПЕРЕМЕЩЕНИЯМИ МЕЖДУ РАДИОКНОПКАМИ/ЧЕКБОКСАМИ С ПОМОЩЬЮ ТАБУЛЯЦИИ ПРИ НАЖАТИИ "ВВОД" ВЫБИРАЕТСЯ ОПЦИЯ
+const dryfood = document.querySelector("#dryfood");
+const wetfood = document.querySelector("#wetfood");
+const naturalfood = document.querySelector("#naturalfood");
+
+const male = document.querySelector("#male");
+const female = document.querySelector("#female");
+
 document
   .querySelector("#drycheckbox")
   .addEventListener("keypress", function (e) {
-    if (
-      e.key === "Enter" &&
-      document.getElementById("dryfood").hasAttribute("checked", "")
-    ) {
-      document.getElementById("dryfood").removeAttribute("checked", "");
+    if (e.key === "Enter" && dryfood.hasAttribute("checked", "")) {
+      dryfood.removeAttribute("checked", "");
     } else if (e.key === "Enter") {
-      document.getElementById("dryfood").setAttribute("checked", "");
+      dryfood.setAttribute("checked", "");
     }
   });
 
 document
   .querySelector("#wetcheckbox")
   .addEventListener("keypress", function (e) {
-    if (
-      e.key === "Enter" &&
-      document.getElementById("wetfood").hasAttribute("checked", "")
-    ) {
-      document.getElementById("wetfood").removeAttribute("checked", "");
+    if (e.key === "Enter" && wetfood.hasAttribute("checked", "")) {
+      wetfood.removeAttribute("checked", "");
     } else if (e.key === "Enter") {
-      document.getElementById("wetfood").setAttribute("checked", "");
+      wetfood.setAttribute("checked", "");
     }
   });
 
 document
   .querySelector("#natcheckbox")
   .addEventListener("keypress", function (e) {
-    if (
-      e.key === "Enter" &&
-      document.getElementById("naturalfood").hasAttribute("checked", "")
-    ) {
-      document.getElementById("naturalfood").removeAttribute("checked", "");
+    if (e.key === "Enter" && naturalfood.hasAttribute("checked", "")) {
+      naturalfood.removeAttribute("checked", "");
     } else if (e.key === "Enter") {
-      document.getElementById("naturalfood").setAttribute("checked", "");
+      naturalfood.setAttribute("checked", "");
     }
   });
 
 document.querySelector("#maleradio").addEventListener("keypress", function (e) {
-  if (
-    e.key === "Enter" &&
-    document.getElementById("male").hasAttribute("checked", "")
-  ) {
-    document.getElementById("male").removeAttribute("checked", "");
+  if (e.key === "Enter" && male.hasAttribute("checked", "")) {
+    male.removeAttribute("checked", "");
   } else {
-    document.getElementById("female").removeAttribute("checked", "");
-    document.getElementById("male").setAttribute("checked", "");
+    female.removeAttribute("checked", "");
+    male.setAttribute("checked", "");
   }
 });
 
 document
   .querySelector("#femaleradio")
   .addEventListener("keypress", function (e) {
-    if (
-      e.key === "Enter" &&
-      document.getElementById("female").hasAttribute("checked", "")
-    ) {
-      document.getElementById("female").removeAttribute("checked", "");
+    if (e.key === "Enter" && female.hasAttribute("checked", "")) {
+      female.removeAttribute("checked", "");
     } else {
-      document.getElementById("male").removeAttribute("checked", "");
-      document.getElementById("female").setAttribute("checked", "");
+      male.removeAttribute("checked", "");
+      female.setAttribute("checked", "");
     }
   });
 
@@ -188,7 +180,7 @@ function uploadFile(file) {
   reader.readAsDataURL(file);
 }
 
-//4- СБРОС ФОТО КОТИКА ПРИ НАЖАТИИ НА КЛАВИШУ "СБРОСИТЬ"
+//4- СБРОС ФОТО КОТИКА И ДАННЫХ ФОРМЫ ПРИ НАЖАТИИ НА КЛАВИШУ "СБРОСИТЬ"
 document.querySelector("#resetForm").addEventListener("click", function () {
   photoPreview.innerHTML = `<img src='assets/img/cat-default.png' alt="photo"'>`;
 });
@@ -197,8 +189,9 @@ document.querySelector("#resetForm").addEventListener("click", function () {
 document.querySelector("#sendForm").addEventListener("click", function (event) {
   event.preventDefault();
 
+  createCatInstance();
+  checkValidity();
   sendForm();
-  createCatCard();
   clearForm();
 });
 
@@ -232,153 +225,80 @@ function addFail() {
   ).innerHTML = `Информация не была отправлена! Проверьте, правильно ли заполнены поля анкеты`;
 }
 
-const clearForm = () => {
+function checkValidity() {
+  console.log("все верно");
+}
+
+function clearForm() {
   console.log("все чисто");
+  document.querySelector("#form").reset();
+
   let inputs = document.querySelectorAll("input");
 
   inputs.forEach(function (input) {
-    if (
-      input.type == "text" ||
-      input.type == "number" ||
-      input.type == "email" ||
-      input.type == "tel"
-    )
-      input.value = "";
-    if (input.type == "checkbox" || input.id == "female") input.checked = false;
-    if (input.id == "male") input.checked = true;
+    if (input.type == "checkbox" || input.id == "female") {
+      input.removeAttribute("checked", "");
+    }
+    if (input.id == "male") {
+      input.addAttribute("checked", "");
+    }
   });
 
-  document.getElementById("race").options[0].selected = true;
-  document.getElementById("comment").value = "";
-
   photoPreview.innerHTML = `<img src='assets/img/cat-default.png' alt="photo"'>`;
-};
+}
 
-//Создание КАРТОЧКИ  и ЭКЗЕМПЛЯРА котика
-const createCatCard = () => {
+//6- СОЗДАНИЕ ЭКЗЕМПЛЯРА КОТИКА, ВЫВОД В КОНСОЛЬ И СОХРАНЕНИЕ В LOCAL STORAGE
+const createCatInstance = () => {
   //1-Собираем данные
   const petname = document.getElementById("petName").value;
   const race = document.querySelector("select[name='race']").value;
-  const maleFemale = document.querySelectorAll('input[name="sex"]');
   const comment = document.querySelector("#comment").value;
 
   let sex = "";
-  let initFood = ""; //Изначально введенное значение
-  let capitalFood; //Изначальное значение с большой буквы
+  let food = [];
 
-  for (let radio of maleFemale) {
+  const dryfood = document.querySelector("#dryfood");
+  const wetfood = document.querySelector("#wetfood");
+  const naturalfood = document.querySelector("#naturalfood");
+
+  for (let radio of document.querySelectorAll('input[name="sex"]')) {
     if (radio.checked) {
       sex = radio.value;
     }
   }
 
-  if (document.querySelector("#dryfood").checked) {
-    initFood += `${document.querySelector("#dryfood").value},`;
+  if (dryfood.checked) {
+    food.push(dryfood.value);
   }
 
-  if (document.querySelector("#wetfood").checked) {
-    initFood += `${document.querySelector("#wetfood").value},`;
+  if (wetfood.checked) {
+    food.push(wetfood.value);
   }
 
-  if (document.querySelector("#naturalfood").checked) {
-    initFood += `${document.querySelector("#naturalfood").value},`;
+  if (naturalfood.checked) {
+    food.push(naturalfood.value);
   }
 
-  if (initFood != "") {
-    capitalFood = initFood[0].toUpperCase() + initFood.slice(1).toLowerCase();
-  } else {
-    capitalFood = "не указано,";
-  }
-
-  //2- Оформляем список данных: первое слово списка с большой буквы, все остальные с маленькой через запятую
-  let separateFood = capitalFood.split(",");
-  let food = separateFood.join(", ").slice(0, -2);
-
-  //3- Создаем экземпляр котика и выводим его в консоль
+  //2- Создаем экземпляр котика и выводим его в консоль
   let myCat = new Cat(petname, race, sex, food, comment, photo);
   console.log("Экземпляр котика:", myCat);
 
-  if (petname && race && sex && food) {
-    //4- Генерируем карточку и добавляем ее на страницу
-    const newCard = generateCard(petname, race, sex, food, comment, photo);
-    document.querySelector("#cat").appendChild(newCard);
-    console.log("добавили карточку котика на страницу");
-
-    //5- Добавляем в локальное хранилище
-    localStorage.setItem(
-      `Cats Collection : ${myCat.petname}`,
-      JSON.stringify(myCat)
-    );
-    console.log("добавили котика в локальное хранилище");
-  }
+  //3- Добавляем в локальное хранилище
+  localStorage.setItem(
+    `Cats Collection : ${myCat.petname}`,
+    JSON.stringify(myCat)
+  );
+  console.log("добавили котика в локальное хранилище");
 };
 
 //КЛАСС КОТИК
 class Cat {
   constructor(petname, race, sex, food, comment, photo) {
     this.petname = petname;
-    this.race = race;
+    this.race = race || "порода не выбрана";
     this.sex = sex;
     this.food = food;
-    this.comment = comment;
-    this.photo = photo;
+    this.comment = comment || "нет комментариев";
+    this.photo = photo || "фото не загружено";
   }
 }
-
-//ГЕНЕРАЦИЯ И ОТРИСОВКА КАРТОЧКИ КОТИКА
-const generateCard = (petname, race, sex, food, comment, photo) => {
-  //Рисуем карточку
-  let card = document.createElement("div");
-  card.classList.add("card");
-
-  let card__image = document.createElement("img");
-  card__image.classList.add("card__image");
-  card__image.src = photo ?? "assets/img/cat-lover.png";
-
-  let card__main = document.createElement("div");
-  card__main.classList.add("card__main");
-
-  let card__info = document.createElement("div");
-  card__info.classList.add("card__info");
-
-  let card__title = document.createElement("h3");
-  card__title.classList.add("card__title");
-  card__title.innerText = petname;
-
-  let card__race = document.createElement("div");
-  card__race.classList.add("card__race");
-  card__race.innerText = `Порода: ${race}`;
-
-  let card__sex = document.createElement("div");
-  card__sex.classList.add("card__sex");
-  card__sex.innerText = `Пол: ${sex}`;
-
-  let card__food = document.createElement("div");
-  card__food.classList.add("card__food");
-  card__food.innerText = `Питание: ${food}`;
-
-  let card__text = document.createElement("p");
-  card__text.classList.add("card__text");
-  card__text.innerText = comment;
-
-  let card__buttons = document.createElement("div");
-  card__buttons.classList.add("card__buttons");
-
-  let card__edit = document.createElement("button");
-  card__edit.classList.add("card__edit");
-  card__edit.innerHTML = "Редактировать";
-
-  card.appendChild(card__image);
-  card.appendChild(card__main);
-
-  card__main.appendChild(card__info);
-  card__main.appendChild(card__text);
-  card__main.appendChild(card__buttons);
-
-  card__info.appendChild(card__title);
-  card__info.appendChild(card__race);
-  card__info.appendChild(card__sex);
-  card__info.appendChild(card__food);
-
-  return card;
-};
